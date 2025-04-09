3b0168e827b5319df4c7e63c21f4347da0633474
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-04-09 22:07:33 +0300

    0904

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=200, label='Заголовок')
+    text = forms.CharField(widget=forms.Textarea, label='Текст поста')
+    author = forms.ModelChoiceField(queryset=User.objects.all(), label='Автор')
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
+
+        labels = {
+            'title': 'Заголовок',
+            'text': 'Текст поста',
+        }
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Post
+from .forms import PostForm
 
 
 def index(request):
-    context = {'title': 'Главная страница'}
+    # получение всех постов (select * from blog_post)
+    posts = Post.objects.all()
+    context = {'title': 'Главная страница', 'posts': posts}
     return render(request, template_name='blog/index.html', context=context)
 
 def about(request):
     context = {'title': 'О сайте'}
     return render(request, template_name='blog/about.html', context=context)
+
+def add_post(request):
+    if request.method == "GET":
+        post_form = PostForm()
+        context = {'title': "Добавить пост",'form':post_form}
+        return render(request, template_name='blog/post_add.html', context=context)
+
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST)
+        if post_form.is_valid():
+            post = Post()
+            post.title = post_form.cleaned_data['title']
+            post.text = post_form.cleaned_data['text']
+            post.author = post_form.cleaned_data['author']
+            post.save()
+            return index(request)
+
+def read_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'title': "Информация о посте", 'post': post}
+    return render(request, template_name='blog/post_detail.html', context=context)
