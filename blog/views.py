1b89fe3384315bba031e4e58b6d7d2919722cff9
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-04-13 15:33:22 +0300

    1304

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
@@ -35,3 +35,22 @@ def read_post(request, pk):
     context = {'title': "Информация о посте", 'post': post}
     return render(request, template_name='blog/post_detail.html', context=context)
 
+def update_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST, files=request.FILES)
+        if post_form.is_valid():
+            post.title = post_form.cleaned_data['title']
+            post.text = post_form.cleaned_data['text']
+            post.author = post_form.cleaned_data['author']
+            post.image = post_form.cleaned_data['image']
+            post.save()
+            return redirect("blog:read_post", pk=post.id)
+    else:
+        post_form = PostForm(initial={
+            "title": post.title,
+            "author": post.author,
+            "text": post.text,
+            "image": post.image,
+        })
+        return render(request, template_name="blog/post_edit.html", context={"form": post_form})
