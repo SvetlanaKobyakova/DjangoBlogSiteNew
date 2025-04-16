8831242836774f322540a8df08a67415f691e1fb
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-04-16 23:35:32 +0300

    1604

--- BlogSite/urls.py
+++ BlogSite/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
